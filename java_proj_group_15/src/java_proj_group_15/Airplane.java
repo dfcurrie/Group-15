8161441c024d1b084c7ea1e13c9e1109e048d850
Rayce Rossum <Rayce.Rossum@gmail.com>
2015-09-17 19:59:40 -0600

    Added command line argument handling and Airplane object

---
+++ java_proj_group_15/src/java_proj_group_15/Airplane.java
@@ -0,0 +1,16 @@
+package java_proj_group_15;
+
+public class Airplane {
+	
+	private int ID, fuel, burnRate, landTime, taxiTime, unloadTime;
+
+	public Airplane(int ID, int fuel, int burnRate, int landTime, int taxiTime, int unloadTime) {
+		this.ID 		= ID;
+		this.fuel 		= fuel;
+		this.burnRate 	= burnRate;
+		this.landTime 	= landTime;
+		this.taxiTime 	= taxiTime;
+		this.unloadTime = unloadTime;
+	}
+
+}
--- java_proj_group_15/src/java_proj_group_15/Main.java
+++ java_proj_group_15/src/java_proj_group_15/Main.java
@@ -3,7 +3,24 @@ package java_proj_group_15;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+    	try {
+	    	if (args[0].equals("-h")) {
+	    		System.out.println("This is a short description of what this program does.");
+	    		System.exit(1);
+	    		
+	    	} else if (args[0].equals("-f") && args[2].equals("-o")) {
+	    		System.out.println("Input: " + args[1] + "\nOutput: " + args[3]);
+	    		
+	    	} else {
+	    		System.out.println("Invalid command line arguments\nProper usage: -f <input_file_name> -o <output_file_name>");
+	    		System.exit(-1);
+	    	}
+	    		
+	 
+    	} catch (ArrayIndexOutOfBoundsException e) {
+    		System.out.println("Invalid command line arguments\nProper usage: -f <input_file_name> -o <output_file_name>");
+    		System.exit(-1);
+    	}
     }
 
 }
