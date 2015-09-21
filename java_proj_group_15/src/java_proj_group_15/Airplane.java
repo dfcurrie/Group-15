1b60ec9f15a47f78f473fcd583b19a8ac6dff4e7
Rayce Rossum <Rayce.Rossum@gmail.com>
2015-09-20 19:16:29 -0600

    Fixed bug in input, stupid colons.

--- java_proj_group_15/src/java_proj_group_15/Airplane.java
+++ java_proj_group_15/src/java_proj_group_15/Airplane.java
@@ -1,15 +1,16 @@
 package java_proj_group_15;
 
 public class Airplane {
-	
+
 	private int ID, fuel, burnRate, landTime, taxiTime, unloadTime;
 
-	public Airplane(int ID, int fuel, int burnRate, int landTime, int taxiTime, int unloadTime) {
-		this.ID 		= ID;
-		this.fuel 		= fuel;
-		this.burnRate 	= burnRate;
-		this.landTime 	= landTime;
-		this.taxiTime 	= taxiTime;
+	public Airplane(int ID, int fuel, int burnRate, int landTime, int taxiTime,
+			int unloadTime) {
+		this.ID = ID;
+		this.fuel = fuel;
+		this.burnRate = burnRate;
+		this.landTime = landTime;
+		this.taxiTime = taxiTime;
 		this.unloadTime = unloadTime;
 	}
 
@@ -60,17 +61,15 @@ public class Airplane {
 	public void setUnloadTime(int unloadTime) {
 		this.unloadTime = unloadTime;
 	}
-	
+
 	@Override
 	public String toString() {
 		String toString = null;
-		
-		toString = new String("ID: " + ID + ", " + fuel +  ", " + burnRate + ", " + landTime + ", " + taxiTime + ", " + unloadTime);
-		
-		
+
+		toString = new String("ID: " + ID + ", " + fuel + ", " + burnRate + ", "
+				+ landTime + ", " + taxiTime + ", " + unloadTime);
+
 		return toString;
-		
-		
 	}
 
 }
--- java_proj_group_15/src/java_proj_group_15/Main.java
+++ java_proj_group_15/src/java_proj_group_15/Main.java
@@ -7,43 +7,48 @@ import java.util.ArrayList;
 
 public class Main {
 
-    public static void main(String[] args) {
-    	try {
-	    	if (args[0].equals("-h")) {
-	    		System.out.println("This is a short description of what this program does.");
-	    		System.exit(1);
-	    		
-	    	} else if (args[0].equals("-f") && args[2].equals("-o")) {
-	    		System.out.println("Input: " + args[1] + "\nOutput: " + args[3]);
-	    		ArrayList<Airplane> airplanes = parseInput();
-	    		System.out.println(airplanes.toString());
-	    		
-	    	} else {
-	    		System.out.println("Invalid command line arguments\nProper usage: -f <input_file_name> -o <output_file_name>");
-	    		System.exit(-1);
-	    	}
-	    		
-	 
-    	} catch (ArrayIndexOutOfBoundsException e) {
-    		System.out.println("Invalid command line arguments\nProper usage: -f <input_file_name> -o <output_file_name>");
-    		System.exit(-1);
-    	}
-    }
-    
-    
-    public static ArrayList<Airplane> parseInput() {	// Returns an array of Airplane objects created from the input file   	
-    	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    	String curLine = null;
-    	ArrayList<Airplane> airplanes = new ArrayList<Airplane>();
-    	int curTuple = 0;
-    	int ID = 0, fuel = 0, burnRate = 0, landTime = 0, taxiTime = 0, unloadTime = 0;
-    	System.out.println("Enter: CASE <caseNumber> to create a new plane");
-    	try {
+	public static void main(String[] args) {
+		try {
+			if (args[0].equals("-h")) {
+				System.out.println(
+						"This is a short description of what this program does.");
+				System.exit(1);
+
+			} else if (args[0].equals("-f") && args[2].equals("-o")) {
+				System.out.println("Input: " + args[1] + "\nOutput: "
+						+ args[3]);
+				ArrayList<Airplane> airplanes = parseInput();
+				System.out.println(airplanes.toString());
+
+			} else {
+				System.out.println(
+						"Invalid command line arguments\nProper usage: -f <input_file_name> -o <output_file_name>");
+				System.exit(-1);
+			}
+
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println(
+					"Invalid command line arguments\nProper usage: -f <input_file_name> -o <output_file_name>");
+			System.exit(-1);
+		}
+	}
+
+	public static ArrayList<Airplane> parseInput() { // Returns an array of Airplane objects created from the input file   	
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				System.in));
+		String curLine = null;
+		ArrayList<Airplane> airplanes = new ArrayList<Airplane>();
+		int curTuple = 0;
+		int ID = 0, fuel = 0, burnRate = 0, landTime = 0, taxiTime = 0,
+				unloadTime = 0;
+		System.out.println("Enter: CASE <caseNumber> to create a new plane");
+		try {
 			while ((curLine = reader.readLine()) != null) {
 				if (curLine.startsWith("CASE")) {
+					curLine = curLine.substring(0, curLine.length() - 1);
 					ID = Integer.parseInt(curLine.substring(5));
 					System.out.println("ID: " + ID);
-				} else if (curLine.equals("END")){
+				} else if (curLine.equals("END")) {
 					break;
 				} else {
 					switch (curTuple) {
@@ -75,19 +80,22 @@ public class Main {
 						break;
 					case 5:
 						curTuple = 0;
-				    	airplanes.add(new Airplane(ID, fuel, burnRate, landTime, taxiTime, unloadTime));	
-				    	System.out.println("New Airplane created: " + ID + fuel + burnRate + landTime + taxiTime + unloadTime);
-				    	System.out.println("Enter CASE <caseNumber> to create a new plane or END to stop creating new planes");
+						airplanes.add(new Airplane(ID, fuel, burnRate, landTime,
+								taxiTime, unloadTime));
+						System.out.println("New Airplane created: " + ID + fuel
+								+ burnRate + landTime + taxiTime + unloadTime);
+						System.out.println(
+								"Enter CASE <caseNumber> to create a new plane or END to stop creating new planes");
 						break;
 					}
 				}
-					
+
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.exit(-1);
 		}
-    	
-    	return airplanes;
-    }
+
+		return airplanes;
+	}
 }
