b9fca1f2a25ea639da502d8000dc3ab61f40618e
Rayce Rossum <Rayce.Rossum@gmail.com>
2015-09-20 19:51:26 -0600

    Input is now threaded

--- java_proj_group_15/src/java_proj_group_15/Main.java
+++ java_proj_group_15/src/java_proj_group_15/Main.java
@@ -17,9 +17,7 @@ public class Main {
 			} else if (args[0].equals("-f") && args[2].equals("-o")) {
 				System.out.println("Input: " + args[1] + "\nOutput: "
 						+ args[3]);
-				ArrayList<Airplane> airplanes = parseInput();
-				System.out.println(airplanes.toString());
-
+					run();
 			} else {
 				System.out.println(
 						"Invalid command line arguments\nProper usage: -f <input_file_name> -o <output_file_name>");
@@ -32,70 +30,15 @@ public class Main {
 			System.exit(-1);
 		}
 	}
-
-	public static ArrayList<Airplane> parseInput() { // Returns an array of Airplane objects created from the input file   	
-		BufferedReader reader = new BufferedReader(new InputStreamReader(
-				System.in));
-		String curLine = null;
-		ArrayList<Airplane> airplanes = new ArrayList<Airplane>();
-		int curTuple = 0;
-		int ID = 0, fuel = 0, burnRate = 0, landTime = 0, taxiTime = 0,
-				unloadTime = 0;
-		System.out.println("Enter: CASE <caseNumber> to create a new plane");
-		try {
-			while ((curLine = reader.readLine()) != null) {
-				if (curLine.startsWith("CASE")) {
-					curLine = curLine.substring(0, curLine.length() - 1);
-					ID = Integer.parseInt(curLine.substring(5));
-					System.out.println("ID: " + ID);
-				} else if (curLine.equals("END")) {
-					break;
-				} else {
-					switch (curTuple) {
-					case 0:
-						fuel = Integer.parseInt(curLine);
-						System.out.println("Fuel: " + fuel);
-						curTuple++;
-						break;
-					case 1:
-						burnRate = Integer.parseInt(curLine);
-						System.out.println("Burn Rate: " + burnRate);
-						curTuple++;
-						break;
-					case 2:
-						landTime = Integer.parseInt(curLine);
-						System.out.println("Land Time: " + landTime);
-						curTuple++;
-						break;
-					case 3:
-						taxiTime = Integer.parseInt(curLine);
-						System.out.println("Taxi Time: " + taxiTime);
-						curTuple++;
-						break;
-					case 4:
-						unloadTime = Integer.parseInt(curLine);
-						System.out.println("Unload Time: " + unloadTime);
-						System.out.println("Press enter to create new plane");
-						curTuple++;
-						break;
-					case 5:
-						curTuple = 0;
-						airplanes.add(new Airplane(ID, fuel, burnRate, landTime,
-								taxiTime, unloadTime));
-						System.out.println("New Airplane created: " + ID + fuel
-								+ burnRate + landTime + taxiTime + unloadTime);
-						System.out.println(
-								"Enter CASE <caseNumber> to create a new plane or END to stop creating new planes");
-						break;
-					}
-				}
-
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(-1);
-		}
-
-		return airplanes;
+	
+	public static void run() {
+		// Read from data file
+		
+		Input input = new Input();  // Start airplane input thread which loads planes
+		input.start();				// into the Input.airplanes list for calculations
+									// based on standard input
+		
+		// Calculate landing scenarios
+		// Print output to screen and file
 	}
 }
