f4a90c0e7b4960cff79e14a318b20e8d15bc09f6
Rayce Rossum <Rayce.Rossum@gmail.com>
2015-09-20 20:47:57 -0600

    Added runway and parking objects

--- java_proj_group_15/src/java_proj_group_15/Main.java
+++ java_proj_group_15/src/java_proj_group_15/Main.java
@@ -14,8 +14,7 @@ public class Main {
 			} else if (args[0].equals("-f") && args[2].equals("-o")) {
 				System.out.println("Input: " + args[1] + "\nOutput: "
 						+ args[3]);
-					run();
-			} else {
+					run(args[1]);
 				System.out.println(
 						"Invalid command line arguments\nProper usage: -f <input_file_name> -o <output_file_name>");
 				System.exit(-1);
@@ -28,16 +27,19 @@ public class Main {
 		}
 	}
 	
-	public static void run() {
+	public static void run(String fileName) {
 		// Read from data file
 		// Create airport and time
+
+		Reader reader = new Reader(fileName);
+		Airport airport = new Airport(Integer.parseInt(reader.getNumRunInput()), Integer.parseInt(reader.getNumParkInput()));
 		Input input = new Input();  			// Start airplane input thread which loads planes
 		input.start();							// into the Input.airplanes list for calculations
 												// based on standard input
 		// Get time from file
 		int time = 1000;
 		Timer timer = new Timer();
-		timer.schedule(new Time(), 0, time);	// Update in-simulation time every few seconds as specified
+		timer.schedule(new Time(), 0, Integer.parseInt(reader.getTimeInput())*1000);	// Update in-simulation time every few seconds as specified
 		// Calculate landing scenarios based on current time
 		// Print output to screen and file
 	}
