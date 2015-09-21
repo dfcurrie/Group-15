d4d8c666c310b22be93b01051a9d7191259d20e7
Rayce Rossum <Rayce.Rossum@gmail.com>
2015-09-20 20:32:01 -0600

    Airport object actually done now

--- java_proj_group_15/src/java_proj_group_15/Airport.java
+++ java_proj_group_15/src/java_proj_group_15/Airport.java
@@ -4,8 +4,24 @@ public class Airport {
 
 	private int runways, parkings;
 	
-	public Airport() {
-		// TODO Auto-generated constructor stub
+	public Airport(int runways, int parkings) {
+		this.runways = runways;
+		this.parkings = parkings;
 	}
 
+	public int getRunways() {
+		return runways;
+	}
+
+	public void setRunways(int runways) {
+		this.runways = runways;
+	}
+
+	public int getParkings() {
+		return parkings;
+	}
+
+	public void setParkings(int parkings) {
+		this.parkings = parkings;
+	}
 }
