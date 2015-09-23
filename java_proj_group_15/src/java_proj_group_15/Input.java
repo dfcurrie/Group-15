b8404e3c3cecc6a9aa2095ea06be14297232d7ec
Rayce Rossum <Rayce.Rossum@gmail.com>
2015-09-23 14:49:53 -0600

    Fixed input formatting

--- java_proj_group_15/src/java_proj_group_15/Input.java
+++ java_proj_group_15/src/java_proj_group_15/Input.java
@@ -4,95 +4,87 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Input extends Thread {
 
 	private ArrayList<Airplane> airplanes;
 	private Time timeTracker;
 	private Airport airport;
-	
+
 	public Input(Time timeTracker, Airport airport) {
 		this.timeTracker = timeTracker;
 		this.airport = airport;
 	}
-	
+
 	@Override
 	public void run() {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				System.in));
 
 		String curLine = null;
 		airplanes = new ArrayList<Airplane>();
-		int curTuple = 0;
-		int ID = 0, fuel = 0, burnRate = 0, landTime = 0, taxiTime = 0, unloadTime = 0;
+		int ID = 0, fuel = 0, burnRate = 0, landTime = 0, taxiTime = 0,
+				unloadTime = 0, caseID = 0;
 		System.out.println("Enter: CASE <caseNumber> to create a new plane");
 		try {
-			while ((curLine = reader.readLine()) != null) {
+			while ((curLine = reader.readLine()) != "END") {
 				if (curLine.startsWith("CASE")) {
 					curLine = curLine.substring(0, curLine.length());
-					ID = Integer.parseInt(curLine.substring(5));
-					System.out.println("ID: " + ID);
-					timeTracker.resetTime();					// Time starts at 0 when Case is entered
-				} else if (curLine.equals("END")) {
+					caseID = Integer.parseInt(curLine.substring(5));
+					System.out.println("CASE ID: " + caseID);
+					timeTracker.resetTime(); // Time starts at 0 when Case is entered
+
+				} else if (curLine.isEmpty()) {
+					// End of scenario, check if can land here
+					Output output = new Output(getAirplanes());
+
+					if (airport.canLand(getAirplanes(), timeTracker
+							.getCurTime()) == true) { // Calculate landing scenarios based on current time
+						// Print output to screen and file with airplane information for input.getAirplanes()
+						System.out.println("Can land all planes");
+						output.runPossible();
+					} else {
+						// Print impossible and exit
+						System.out.println("Can not land all planes");
+						output.runImpossible();
+					}
+				} else if (curLine.startsWith("END")) {
 					break;
 				} else {
-					switch (curTuple) {
-					case 0:
-						fuel = Integer.parseInt(curLine);
-						System.out.println("Fuel: " + fuel);
-						curTuple++;
-						break;
-					case 1:
-						burnRate = Integer.parseInt(curLine); // switched with
-																// print line
-						System.out.println("Burn Rate: " + burnRate); // added +
-																		// burnRate
-						curTuple++;
-						break;
-					case 2:
-						landTime = Integer.parseInt(curLine);
-						System.out.println("Land Time: " + landTime);
-						curTuple++;
-						break;
-					case 3:
-						taxiTime = Integer.parseInt(curLine);
-						System.out.println("Taxi Time: " + taxiTime);
-						curTuple++;
-						break;
-					case 4:
-						unloadTime = Integer.parseInt(curLine);
-						System.out.println("Unload Time: " + unloadTime);
-						System.out.println("\nPress enter to create new plane");
-						curTuple++;
-						break;
-					case 5:
-						curTuple = 0;
-						airplanes.add(new Airplane(ID, fuel, burnRate, landTime, taxiTime, unloadTime));
-						System.out.println(
-								"New Airplane created: " + ID + fuel + burnRate + landTime + taxiTime + unloadTime);
-						// End of scenario, check if can land here
-						Output output = new Output(getAirplanes());
-						if (airport.canLand(getAirplanes(), timeTracker.getCurTime()) == true) { // Calculate landing scenarios based on current time
-							// Print output to screen and file with airplane information for input.getAirplanes()
-							System.out.println("Can land all planes");
-							output.runPossible();
-						} else {
-							// Print impossible and exit
-							System.out.println("Can not land all planes");
-							output.runImpossible();
-						}
-						
-						System.out.println(
-								"Enter CASE <caseNumber> to create a new plane or END to stop creating new planes");
-						break;
-					}
+					curLine = curLine.substring(1, curLine.length() - 1); // Remove ()
+
+					List<String> planeInfo = Arrays.asList(curLine.split(
+							"\\s*,\\s*"));
+					System.out.println(planeInfo.toString());
+
+					ID = Integer.parseInt(planeInfo.get(0));
+					fuel = Integer.parseInt(planeInfo.get(1));
+					burnRate = Integer.parseInt(planeInfo.get(2));
+					landTime = Integer.parseInt(planeInfo.get(3));
+					taxiTime = Integer.parseInt(planeInfo.get(4));
+					unloadTime = Integer.parseInt(planeInfo.get(5));
+
+					airplanes.add(new Airplane(ID, fuel, burnRate, landTime,
+							taxiTime, unloadTime));
+					System.out.println("New Airplane created: " + ID + fuel
+							+ burnRate + landTime + taxiTime + unloadTime);
+
 				}
 			}
-		} catch (IOException e) {
+
+		} catch (
+
+		IOException e)
+
+		{
 			e.printStackTrace();
 			System.exit(-1);
 		}
 
 		System.out.println(airplanes);
+
 	}
 
 	public ArrayList<Airplane> getAirplanes() {
