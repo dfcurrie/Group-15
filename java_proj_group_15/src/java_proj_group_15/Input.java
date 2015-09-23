ba3164622d0cdbcc6d6687b277ca3a5e83f50212
dfcurrie <dfcurrie@ucalgary.ca>
2015-09-23 11:59:38 -0600

    added the ability to have output file but needs work

--- java_proj_group_15/src/java_proj_group_15/Input.java
+++ java_proj_group_15/src/java_proj_group_15/Input.java
@@ -5,6 +5,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.io.FileWriter;
+import java.io.File;
 
 public class Input extends Thread {
 
@@ -20,12 +22,16 @@ public class Input extends Thread {
 	@Override
 	public void run() {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
 		String curLine = null;
+		String toFile = "CASE:";
 		airplanes = new ArrayList<Airplane>();
 		int curTuple = 0;
 		int ID = 0, fuel = 0, burnRate = 0, landTime = 0, taxiTime = 0, unloadTime = 0;
 		System.out.println("Enter: CASE <caseNumber> to create a new plane");
 		try {
+			File file = new File("outputFile"); //////Need to file a way to pass the outputfile given to
+			FileWriter fw = new FileWriter(file); /// input
 			while ((curLine = reader.readLine()) != null) {
 				if (curLine.startsWith("CASE")) {
 					curLine = curLine.substring(0, curLine.length());
@@ -68,7 +74,9 @@ public class Input extends Thread {
 						airplanes.add(new Airplane(ID, fuel, burnRate, landTime, taxiTime, unloadTime));
 						System.out.println(
 								"New Airplane created: " + ID + fuel + burnRate + landTime + taxiTime + unloadTime);
-						
+						toFile = toFile +","+ ID +","+ fuel +","+ burnRate +","+ landTime +","+ taxiTime +","+ unloadTime;
+						fw.write(toFile);
+						fw.close();
 						// End of scenario, check if can land here
 						if (airport.canLand(getAirplanes(), timeTracker.getCurTime()) == true) { // Calculate landing scenarios based on current time
 							// Print output to screen and file with airplane information for input.getAirplanes()
