39fbdaab39c14106fca5ca007f78c9ba74118274
Rayce Rossum <Rayce.Rossum@gmail.com>
2015-09-23 12:19:09 -0600

    Commented code that caused crash upon entering second plane

--- java_proj_group_15/src/java_proj_group_15/Input.java
+++ java_proj_group_15/src/java_proj_group_15/Input.java
@@ -24,14 +24,14 @@ public class Input extends Thread {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
 		String curLine = null;
-		String toFile = "CASE:";
+//		String toFile = "CASE:";
 		airplanes = new ArrayList<Airplane>();
 		int curTuple = 0;
 		int ID = 0, fuel = 0, burnRate = 0, landTime = 0, taxiTime = 0, unloadTime = 0;
 		System.out.println("Enter: CASE <caseNumber> to create a new plane");
 		try {
-			File file = new File("outputFile"); //////Need to file a way to pass the outputfile given to
-			FileWriter fw = new FileWriter(file); /// input
+//			File file = new File("outputFile"); //////Need to file a way to pass the outputfile given to
+//			FileWriter fw = new FileWriter(file); /// input
 			while ((curLine = reader.readLine()) != null) {
 				if (curLine.startsWith("CASE")) {
 					curLine = curLine.substring(0, curLine.length());
@@ -74,9 +74,9 @@ public class Input extends Thread {
 						airplanes.add(new Airplane(ID, fuel, burnRate, landTime, taxiTime, unloadTime));
 						System.out.println(
 								"New Airplane created: " + ID + fuel + burnRate + landTime + taxiTime + unloadTime);
-						toFile = toFile +","+ ID +","+ fuel +","+ burnRate +","+ landTime +","+ taxiTime +","+ unloadTime;
-						fw.write(toFile);
-						fw.close();
+//						toFile = toFile +","+ ID +","+ fuel +","+ burnRate +","+ landTime +","+ taxiTime +","+ unloadTime;
+//						fw.write(toFile);
+//						fw.close();
 						// End of scenario, check if can land here
 						if (airport.canLand(getAirplanes(), timeTracker.getCurTime()) == true) { // Calculate landing scenarios based on current time
 							// Print output to screen and file with airplane information for input.getAirplanes()
