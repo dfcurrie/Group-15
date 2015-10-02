b6ee89aafc54ebd8f63c87c119f4b1d50b260d94
Rayce Rossum <Rayce.Rossum@gmail.com>
2015-10-02 14:47:34 -0600

    ASN VAR and Mem now working

--- haskell_expression_evaluation/src/Expressions.hs
+++ haskell_expression_evaluation/src/Expressions.hs
@@ -17,19 +17,25 @@ type Mem = [(String, Int)]
 -- returns the result of the computation and the updated memory
 eval :: Exp -> Mem -> (Int, Mem)
 eval (CONST cnst) mem = (cnst, mem)
-eval (ADD ex y) mem = (fst(eval ex mem) + fst(eval y mem), mem)
-eval (SUB ex y) mem = (fst(eval ex mem) - fst(eval y mem), mem)
-eval (MUL ex y) mem = (fst(eval ex mem) * fst(eval y mem), mem)
-eval (DIV ex y) mem = (fst(eval ex mem) `div` fst(eval y mem), mem)
-eval (ASN strng ex) mem = (fst(eval ex mem), (strng, fst(eval ex mem)):mem)
+eval (ADD ex y) mem = (fst(eval ex mem) + fst(eval y mem), snd(eval ex mem) ++ snd(eval y mem))
+eval (SUB ex y) mem = (fst(eval ex mem) - fst(eval y mem), snd(eval ex mem) ++ snd(eval y mem))
+eval (MUL ex y) mem = (fst(eval ex mem) * fst(eval y mem), snd(eval ex mem) ++ snd(eval y mem))
+eval (DIV ex y) mem = (fst(eval ex mem) `div` fst(eval y mem), snd(eval ex mem) ++ snd(eval y mem))
+eval (ASN strng ex) mem = (fst(eval ex mem), (replaceVar (strng, fst(eval ex mem)) mem))
 eval (VAR strng) mem = (ridMaybeInt 0 (lookup strng mem), mem)
-		
-		
+
+
 -- A function that converts a Maybe Int into an Int if the Maybe Int has an Int
 -- else it returns a default provided by it's first argument
 ridMaybeInt :: Int -> Maybe Int -> Int
 ridMaybeInt a Nothing = a
 ridMaybeInt a (Just b) = b
+
+replaceVar :: (String, Int) -> Mem -> Mem
+replaceVar x [] = [x] 
+replaceVar (str,y) (x:xs)
+    | str == fst(x) = ((str, y):xs)
+    | otherwise = x:(replaceVar (str, y) xs)
 
 
 -- A function that runs a series of expressions (i.e. a program) given a memory
