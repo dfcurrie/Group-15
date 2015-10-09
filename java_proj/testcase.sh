7400ddf4ec64fc4a2a643b56285e994b89380320
kpratt <keynan.pratt@gmail.com>
2015-10-09 13:14:38 -0600

    grading delta

--- java_proj/testcase.sh
+++ java_proj/testcase.sh
@@ -1,105 +1,61 @@
-#! /bin/bash
-
-
-sleep 2
-
-# =========================================================
-# simple
 echo "CASE 1:"
+sleep 15
 echo "(1,2,1,1,1,2)"
-echo ""
-sleep 20
 
-
-# =========================================================
-# wait in air
 echo "CASE 2:"
+sleep 15
 echo "(1,10,1,1,1,1)"
 echo "(2,10,1,1,1,1)"
 echo "(3,10,1,1,1,1)"
-echo ""
-sleep 20
 
-
-# =========================================================
-# no wait (long enough delay)
 echo "CASE 3:"
+sleep 15
 echo "(1,10,1,1,1,4)"
-sleep 22
+sleep 120
 echo "(2,10,1,1,1,1)"
-sleep 20
+sleep 120
 echo "(3,10,1,1,1,1)"
-echo ""
-sleep 20
 
-
-# =========================================================
-# no wait (very close delay)
 echo "CASE 4:"
+sleep 15
 echo "(1,10,1,1,1,4)"
-sleep 6
-echo "(2,10,1,1,1,5)"
-sleep 4
-echo "(3,10,1,1,3,2)"
-echo ""
 sleep 30
+echo "(2,10,1,1,1,5)"
+sleep 30
+echo "(3,10,1,1,3,2)"
 
-
-# =========================================================
-# more in each time unit (beginning & end)
 echo "CASE 5:"
-sleep 1
+sleep 15
+sleep 3
 echo "(1,10,1,1,1,1)"
-sleep 2
+sleep 24
 echo "(2,10,1,1,1,1)"
-echo ""
-sleep 20
 
-
-# =========================================================
-# fuel fraction success
 echo "CASE 6:"
+sleep 15
 echo "(1,10,3,1,1,4)"
-echo ""
-sleep 20
 
-
-# =========================================================
-# fuel fraction crash
 echo "CASE 7:"
+sleep 15
 echo "(1,5,3,1,1,4)"
-sleep 10
+sleep 120
 echo "(2,10,1,1,1,1)"
-echo ""
-sleep 5
 
-
-# =========================================================
-# fuel 0
 echo "CASE 8:"
+sleep 15
 echo "(1,0,1,1,1,4)"
-echo ""
-sleep 5
 
-
-# =========================================================
-#parking crash
 echo "CASE 9:"
+sleep 15
 echo "(1,41,2,1,5,3)"
 echo "(2,41,2,1,5,3)"
 echo "(3,90,1,1,1,10)"
-echo ""
-sleep 52
 
-
-# =========================================================
-#crash in air
 echo "CASE 10:"
+sleep 15
 echo "(1,10,1,1,1,1)"
 echo "(2,10,1,1,1,1)"
 echo "(3,1,1,1,1,1)"
-echo ""
-sleep 52
 
-
+sleep 60
 echo "END"
--- java_proj/tester.sh
+++ java_proj/tester.sh
@@ -1,3 +1,9 @@
 #! /bin/bash
 
-./testcase.sh | java -cp bin/ `cat MAIN_CLASS` -f input.txt -o output.txt
+./testcase.sh | java -cp bin/ `cat MAIN_CLASS` -f input.txt -o output.txt &
+PID=$!
+echo $PID
+sleep 1200
+kill $PID
+
+echo "DONE\n"
