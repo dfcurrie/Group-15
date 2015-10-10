15c33589470e048e628fcb3d1af462bba4a3c824
kpratt <keynan.pratt@gmail.com>
2015-10-10 16:09:58 -0600

    partial

--- java_proj/tester.sh
+++ java_proj/tester.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
-./testcase.sh | java -cp bin/ `cat MAIN_CLASS` -f input.txt -o output.txt &
+./testcase.sh | java -cp bin/ `cat MAIN_CLASS` -f input.txt -o output.txt > STDOUT 2> STDERROR &
 PID=$!
 echo $PID
 sleep 1200
 kill $PID
+if [ $? -ne 0 ] ; then
+    echo "Killed `pwd`"
+fi
 
-echo "DONE\n"
