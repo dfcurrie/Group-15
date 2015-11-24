0420199e4f0250145f673f58c3328fe6433c607f
Rayce Rossum <Rayce.Rossum@gmail.com>
2015-11-24 16:50:34 -0700

    Did some of the easy rules

--- prolog_assignment/family_reasoning.pl
+++ prolog_assignment/family_reasoning.pl
@@ -1,15 +1,24 @@
-%parentOf(A, B).
-%motherOf(A, B).
-%fatherOf(A, B).
-%grandparentOf(A, B).
-%grandmotherOf(A, B).
-%grandfatherOf(A, B).
+% Notations
+% +: Shows that the argument must be instantiated.
+% -: Shows that the argument must be a variable (will be instantiated if the built-in predicate succeeds).
+% ?: Shows the argument can be instantiated or a variable.
+% strict: The result, may not have duplicate values.
+% non-strict: The result can contain duplicate values.
+% bonus: Implementation is optional and successful implementation is rewarded with extra points.
+% fact vs. rules: Consult lecture notes for the difference between the two.
+
+parentOf(A, B) :- hasChild(A, X).
+motherOf(A, B) :- hasChild(A, X), female(A).
+fatherOf(A, B) :- hasChild(A, X), male(A).
+grandparentOf(A, B) :- parentOf(A, X), parentOf (X, B).
+grandmotherOf(A, B) :- motherOf(A, X), motherOf(X, B).
+grandfatherOf(A, B) :- fatherOf(A, X), fatherOf(X, B).
 %greatgrandparentOf(A, B).
 %greatgrandmotherOf(A, B).                            
 %greatgrandfatherOf(A, B).                            
-%childOf(A, B).
-%daughterOf(A, B).
-%sonOf(A, B).
+childOf(A, B) :- parentOf(B, A).
+daughterOf(A, B) :- parentOf(B, A), female(A).
+sonOf(A, B) :- parentOf(B, A), male(A).
 %grandchildOf(A, B).
 %granddaughterOf(A, B).
 %grandsonOf(A, B).
