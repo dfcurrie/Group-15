a268b4be2614c3bfd9ff1b92acee0d6651f25765
Rayce Rossum <Rayce.Rossum@gmail.com>
2015-11-24 16:04:01 -0700

    Template commit

---
+++ prolog_assignment/family_reasoning.pl
@@ -0,0 +1,36 @@
+%parentOf(A, B).
+%motherOf(A, B).
+%fatherOf(A, B).
+%grandparentOf(A, B).
+%grandmotherOf(A, B).
+%grandfatherOf(A, B).
+%greatgrandparentOf(A, B).
+%greatgrandmotherOf(A, B).                            
+%greatgrandfatherOf(A, B).                            
+%childOf(A, B).
+%daughterOf(A, B).
+%sonOf(A, B).
+%grandchildOf(A, B).
+%granddaughterOf(A, B).
+%grandsonOf(A, B).
+%greatgrandchildOf(A, B).
+%greatgranddaughterOf(A, B).
+%greatgrandsonOf(A, B).
+%ancestorOf(A, B).
+%ancestorOf(A, B, N).
+%descendantOf(A, B, N).
+%related(A, B).
+%parent(A).
+%childless(A).
+%hasChildren(A, L).
+%countChildren(A, N).
+%sibling(X, Y).
+%sisterOf(A, B).
+%brotherOf(A, B).
+%stepSibling(A, B).
+%stepSisterOf(A, B).
+%stepBrotherOf(A, B).
+%cousin(A, B).
+%getSpecies(A, B).
+%pet(A).
+%feral(A).
